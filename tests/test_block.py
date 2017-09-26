5852be9f0b57bcca4627b45c749b20fa0e7f934d
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-09-26 02:25:22 -0700

    TST: Tests for Watchblock

--- tests/test_block.py
+++ tests/test_block.py
@@ -34,6 +34,8 @@ def test_block_watch_soft_trip(watchblock):
 
     #Cause a trip, but do not close the blocker
     watchblock.ai._read_pv.put(0)
+    #Process event
+    watchblock.process_event()
     assert watchblock.soft_trip.get() == 1
     assert watchblock.hard_trip.get() == 0
     #Filter did not close
@@ -43,11 +45,14 @@ def test_block_watch_soft_trip(watchblock):
 def test_block_watch_hard_trip(watchblock):
     #Start enabled and sequencer on
     watchblock.enabled._read_pv.put(1)
-    watchblock.seq_run._read_pv.put(1)
+    watchblock.seq_run._read_pv.put(2)
+    watchblock.seq_cnt._read_pv.put(50)
     assert watchblock.sequencer_running
 
     #Cause a trip, and close the blocker
     watchblock.ai._read_pv.put(0)
+    #Process event
+    watchblock.process_event()
     assert watchblock.soft_trip.get() == 0
     assert watchblock.hard_trip.get() == 1
     #Filter closed
@@ -55,6 +60,8 @@ def test_block_watch_hard_trip(watchblock):
 
     #Recover
     watchblock.ai._read_pv.put(5)
+    #Process event
+    watchblock.process_event()
     assert watchblock.soft_trip.get() == 0
     assert watchblock.hard_trip.get() == 0
     #Filter re-opened    
@@ -64,11 +71,14 @@ def test_block_watch_hard_trip(watchblock):
 def test_block_watch_disabled(watchblock):
     #Start disabled and sequencer off
     watchblock.enabled._read_pv.put(0)
-    watchblock.seq_run._read_pv.put(1)
+    watchblock.seq_run._read_pv.put(2)
+    watchblock.seq_cnt._read_pv.put(50)
     assert watchblock.sequencer_running
 
     #Cause a trip, but do not close the blocker
     watchblock.ai._read_pv.put(0)
+    #Process event
+    watchblock.process_event()
     assert watchblock.soft_trip.get() == 0
     assert watchblock.hard_trip.get() == 1
     #Filter did not close
